20a3b088c8c1f276e83b41e65cc2e8021b09397d
CamiloQuintana2005 <Camilo.Quintana.1290@gmail.com>
2025-11-17 03:43:16 -0300

    Add Product model and ProductService with HttpClient

---
+++ src/app/core/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private url = 'assets/data/products.json';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url);
+  }
+
+  getById(id: number): Observable<Product | undefined> {
+    return this.getAll().pipe(
+      map(products => products.find(p => p.id === id))
+    );
+  }
+}
